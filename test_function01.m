37d598f3014325995f7953ac65315de29356935d
Jojo Liu <covenanter32@gmail.com>
2025-09-22 22:25:15 -0400

    various test functions

--- test_function01.m
+++ test_function01.m
@@ -1,5 +1,4 @@
 function [f_val, jacobian] = test_function01(x)
-    % test vector valued function for multivariate_newton_solver()
     f_val = [x(1)^2 + x(2)^2 - 6 - x(3)^5;
              x(1)*x(3) + x(2) - 12;
              sin(x(1) + x(2) + x(3))];
