ed8a2fb12ae78361f8a6b1e4afc57c562edfe6ed
Jojo Liu <covenanter32@gmail.com>
2025-09-22 22:24:13 -0400

    h is larger for better numerical sol'n

--- approximate_jacobian.m
+++ approximate_jacobian.m
@@ -5,12 +5,12 @@ function approx_j = approximate_jacobian(fun, x)
 
     J = zeros(output_dim, input_dim);
 
-    h = 1e-8;
+    h = 1e-6;
     
     for i=1:input_dim
         std_basis_vec = zeros(input_dim, 1);
-        std_basis_vec(i) = h;
-        J(:, i) = (fun(x + std_basis_vec) - fun(x - std_basis_vec)) / (2*h);
+        std_basis_vec(i) = h/2;
+        J(:, i) = (fun(x + std_basis_vec) - fun(x - std_basis_vec)) / h;
     end
 
     approx_j = J;
